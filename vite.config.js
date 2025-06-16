88cb6ad415029bcb0e02abeda93b3f2b858dbc7a
abdoulaye150199 <abdoualaye150199@gmail.com>
2025-06-16 12:23:57 +0000

    OK

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,24 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './',
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: './index.html',
-        login: './login.html',
-        register: './register.html'
+        main: 'index.html'
       },
       output: {
-        manualChunks: {
-          vendor: ['./src/utils', './src/components'],
-          main: ['./src/main.js'],
-          auth: ['./src/controllers/authController.js']
-        },
-        chunkFileNames: 'assets/[name]-[hash].js',
+        format: 'es',
         entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
         assetFileNames: 'assets/[name]-[hash][extname]'
       }
     },
-    minify: 'terser',
-    sourcemap: false,
-    cssCodeSplit: false
+    target: 'es2015',
+    sourcemap: false
   },
   server: {
     port: 5173,
-    open: true
-  },
-  optimizeDeps: {
-    exclude: ['json-server']
+    host: true
   }
 })
